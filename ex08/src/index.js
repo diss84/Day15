ba553cf99ed04539284692c0a40b225db8560386
Mirela <adisserdarevic@gmail.com>
2021-03-22 21:38:42 +0100

    Added file index.js

---
+++ ex08/src/index.js
@@ -0,0 +1,8 @@
+// only change code below this line
+const sum = (num1, num2 = 1) => {
+    return num1+num2;
+}
+console.log(sum(4, 6)); // change this line
+console.log(sum(3));
+
+module.exports = sum;
