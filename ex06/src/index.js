11535fac551526e00b495c0e8f480be75b2ebc1e
Mirela <adisserdarevic@gmail.com>
2021-03-22 21:02:53 +0100

    Added file index.js

---
+++ ex06/src/index.js
@@ -0,0 +1,9 @@
+// only change code below this line
+const arena = () => {
+    "use strict";
+    return new Date();
+};
+// only change code below this line
+console.log(arena());
+
+module.exports = arena;
